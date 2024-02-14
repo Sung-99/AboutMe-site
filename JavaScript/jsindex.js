61e0a5c4f35543ca9d9ba83e3b96b11cdc6e11f8
Sung-99 <vitorhugomota99@hotmail.com>
2024-02-14 17:02:23 -0300

    New visual up to date

--- JavaScript/jsindex.js
+++ JavaScript/jsindex.js
@@ -132,8 +132,49 @@
 
  /* Só um horario no meu html com setinterval mudando horas, minutos e segundos, ele vai atualizar os 3 a cada segundo,
  mas como só o segundo muda a cada segundo(obviamente), só ele será */
-setInterval(() => {
+
+
+ let day = ['Segunda','Terça','Quarta','Quinta','Sexta','Sabado','Domingo'];
+
+ setInterval(function (){
   let date = new Date();
-  let time =(date.getHours()) + ':' + ('0' + date.getMinutes()).slice(-2) + ':' + ('0' + date.getSeconds()).slice(-2) + ' ' + (date.getHours() >= 12 ? 'PM' : 'AM');
-  document.getElementById('clockDisplay').textContent = time;
-}, 1000);
+  let x = date.getDay()
+  document.getElementById('clockDisplay').textContent = day[x-1] + ' ' + date.getHours() +':'+('0'+ date.getMinutes()).slice(-2) +':'+ ('0' + date.getSeconds()).slice(-2);
+
+ }, 1000);
+  
+
+
+ /*------------------Slide in js--------------*/
+
+ let slider = document.getElementById('slider');
+
+ let outerSliderPlaceHolder = document.querySelector(".outer_slider_in");
+ /*
+
+ ***Use as example: https://stackoverflow.com/questions/16989585/css-3-slide-in-from-left-transition
+ 
+ */
+
+
+ 
+ slider.addEventListener('mouseleave', function() {
+     
+     if(slider.classList.contains('slide-in')){
+      slider.setAttribute('class','slide-out');
+      
+     }
+    
+ });
+
+ 
+
+ /*---------------------Click again to show personal info btn */
+ outerSliderPlaceHolder.addEventListener('mouseenter',function(){
+  if(slider.classList.contains('slide-out') && 
+  (outerSliderPlaceHolder.classList.contains('outer_slider_in')))
+  {
+    slider.setAttribute('class','slide-in');
+  
+   }
+ });
